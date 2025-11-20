741b3b0f9696feddf60fcc919711cb480957c256
Luiz Henrique Murback Wiedmer <112104065+lhwiedmer@users.noreply.github.com>
2025-11-20 01:37:06 -0300

    Change mppSort parameter from 1 to 10
    Updated the execution parameters for mppSort to use 10 instead of 1 for multiple test cases.

--- script.sh
+++ script.sh
@@ -6,21 +6,21 @@
 # Executa os programas compilados
 echo "Executando aquecimento"
 
-./mppSort 1000000 1024 1
+./mppSort 1000000 1024 10
 
 echo "Executando teste 1M"
 
-./mppSort 1000000 1024 1
+./mppSort 1000000 1024 10
 
 echo "Executando teste 2M"
 
-./mppSort 2000000 1024 1
+./mppSort 2000000 1024 10
 
 echo "Executando teste 4M"
 
-./mppSort 4000000 1024 1
+./mppSort 4000000 1024 10
 
 echo "Executando teste 8M"
 
-./mppSort 8000000 1024 1
+./mppSort 8000000 1024 10
 
